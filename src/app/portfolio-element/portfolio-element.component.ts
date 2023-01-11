1dc16e6ad46b3288a6e990f52416e15b60a35894
Norbert <norbrider87@gmail.com>
2023-01-11 13:21:36 +0100

    portfolio element structure change

--- src/app/portfolio-element/portfolio-element.component.ts
+++ src/app/portfolio-element/portfolio-element.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-portfolio-element',
   templateUrl: './portfolio-element.component.html',
   styleUrls: ['./portfolio-element.component.scss']
 })
 export class PortfolioElementComponent implements OnInit {
+
+  
+
+  projects = [
+    {
+      name: "Join",
+      techstack: "Angular | Typescript | Html | CSS | Firebase",
+      img: "../../assets/img/join.png",
+      description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+      linktogit: "google.com",
+      row : false,
+    },
+    {
+      name: "Join",
+      techstack: "Angular | Typescript | Html | CSS | Firebase",
+      img: "../../assets/img/join.png",
+      description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+      linktogit: "google.com",
+      row: true,
+    }
+  ];
 
   constructor() { }
 
